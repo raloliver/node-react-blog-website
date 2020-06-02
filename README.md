782ab8b349b79587d58890929daa6a4e17b7a4ae
raloliver <contato@raloliver.com.br>
2020-06-02 17:15:19 +0100

    Add like post to simulate increment likes

--- README.md
+++ README.md
@@ -5,4 +5,6 @@
 ### Dependencies
 
 - It is important to use @babel packages because we use ES2015+ sintax on our code.
-- Also, it is important to use body-parser to handle with post action.
+- Also, it is important to use body-parser to handle with post action. `express().use(bodyParser.json())` and to get a value, just use `app.post('/', (req, res) => res.send(`${req.body.name}!`))`
+- To get value from params, use `express.get('/:value', (req, res) => res.send(`${req.params.vale}!`))` when you need to pass by url.
+- Use `nodemon` if you dont restart server every time you made a change.
--- src/server.js
+++ src/server.js
@@ -1,12 +1,31 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 
+const fakePosts = {
+    'post-0': {
+        like: 0
+    },
+    'post-1': {
+        like: 0
+    },
+    'post-2': {
+        like: 0
+    }
+}
 
 const app = express();
 
 app.use(bodyParser.json());
 
+app.post('/api/posts/:name/like', (req, res) => {
+    const post = req.params.name;
+
+    fakePosts[post].like += 1;
+    res.status(200).send(`${post} now has ${fakePosts[post].like} like(s)`)
+});
+
 app.get('/', (req, res) => res.send('Hello'));
+app.get('/:name', (req, res) => res.send(`Hello ${req.params.name}!`));
 app.post('/', (req, res) => res.send(`Hello ${req.body.name}!`));
 
 app.listen(8000, () => console.log('8000 started...'));
